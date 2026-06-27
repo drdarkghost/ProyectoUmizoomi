3f1f9222522f2ea96870ac3649983f8d9ea5b6a5
tacosdemichi001 <tacosdemichi001@gmail.com>
2026-06-27 14:30:56 -0600

    cell

--- src/cr/ac/ucr/paraiso/model/Cell.java
+++ src/cr/ac/ucr/paraiso/model/Cell.java
@@ -1,5 +1,17 @@
 package cr.ac.ucr.paraiso.model;
 //arreglo
 public class Cell {
+private Item item;
 
+    public Cell() {
+        this.item = null;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
 }
